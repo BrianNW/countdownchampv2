4471b96de679da4433a97b81c1c5bbeda664d5ca
Brian W <Briantechdom@gmail.com>
2018-03-30 21:23:58 -0700

    Update with complete project files.  Not packaged for public.

--- countdownchampv2/src/App.jsx
+++ countdownchampv2/src/App.jsx
@@ -1,23 +1,44 @@
 import React, { Component } from 'react';
+import Clock from './Clock';
 import './App.css';
+import { Form, FormControl, Button} from 'react-bootstrap';
 
 class App extends Component {
-  render() {
+  constructor(props) {
+    super(props);
+    this.state = {
+      deadline: 'December 25, 2018',
+      newDeadline: ''
+    }
+  }
+
+  changeDeadline() {
+    this.setState({deadline: this.state.newDeadline})
+
+  }
+
+  render() { //render method allows us to return any JSX we would like
     return (
+      //Turns this Div into it's own component. Must declare export at the bottom of file
     <div className="App">
-      <div className="App-title"> Countdown to December 25, 2018</div>
-      <div>
-        <div> 14 days </div>
-        <div> 30 hours</div>
-        <div> 15 minutes</div>
-        <div> 20 Seconds </div>
+      <div className="App-title">
+        Countdown to {this.state.deadline}
       </div>
-        <div>
-          <input placeholder='new date' />
-          <button> Submit</button>
-        </div>
+      <Clock
+        deadline = {this.state.deadline}
+      />
+        <Form inline>
+          <FormControl
+          className="Deadline-input"
+          placeholder='new date'
+          onChange={event => this.setState({newDeadline: event.target.value })}
+          />
+          <Button onClick={() => this.changeDeadline()}>
+           Submit
+          </Button>
+        </Form>
     </div>
-  )
+   )
   }
 }
 
--- countdownchampv2/src/index.js
+++ countdownchampv2/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './App'; // Needed to be able to use App component
 
 ReactDOM.render(
   <App />,
